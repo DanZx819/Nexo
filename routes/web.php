e8c1d8ed661bed4ccabdc9fd4be5f89e57f3f3b2
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-17 17:14:53 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -19,7 +22,6 @@ Route::middleware(['guest'])->group(function () {
 
     //Rotas de Login
 
-
     Route::get('/login', LoginController::class)->name('login');
 
     Route::post('/login', [LoginController::class, 'loginIn'])->name('login-in');
@@ -30,6 +32,18 @@ Route::middleware('auth')->group(function (){
 
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
+    Route::get('/produtos/{produto}', [ProductController::class, 'show'])->name('produtos.show');
+
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     
+});
+
+Route::middleware(IsAdmin::class)->group(function (){
+    Route::get('/product/create', [ProductController::class, 'create'])->name('create.index');
+
+    Route::post('/product/create', [ProductController::class, 'store'])->name('store.product');
+
+    Route::get('/menu', AdminController::class)->name('admin.menu');
+
+    Route::delete('/producos/delete/{produto}', [ProductController::class, 'destroy'])->name('product.delete');
 });
