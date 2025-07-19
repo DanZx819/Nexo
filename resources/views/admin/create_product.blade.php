f38efb131a3528b94db4d9f2d4ed8ceecffb4a37
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-18 22:35:11 -0300

    Update create_product.blade.php

--- resources/views/admin/create_product.blade.php
+++ resources/views/admin/create_product.blade.php
@@ -4,25 +4,29 @@
 
 
 @section('content')
-<div class="flex justify-center items-center min-h-screen">
+    <div class="flex justify-center items-center min-h-screen">
 
-    
-    <form action="{{ route('store.product') }}" method="post" enctype="multipart/form-data" class="flex flex-col w-3/12 space-y-2">
-        @csrf
-        <h1 class="text-xl">Create a new Product</h1>
 
-        
+        <form action="{{ route('store.product') }}" method="post" enctype="multipart/form-data"
+            class="flex flex-col w-3/12 space-y-2">
+            @csrf
+            <h1 class="text-xl">Create a new Product</h1>
 
-        <input type="text" name="titulo" required class="input input-info" placeholder="Title">
 
-        <input type="text" name="descricao" required class="input input-info" placeholder="Description">
 
-        <input type="number" name="preco" step="0.01" required class="input input-info" placeholder="Price">
+            <input type="text" name="titulo" required class="input input-info" placeholder="Title">
 
-        <input type="file" name="foto" required class="file-input file-input-info">
+            <input type="text" name="descricao" required class="input input-info" placeholder="Description">
 
-        <button type="submit" class="btn btn-outline btn-info w-25 hover:text-white">Create</button>
-    </form>
-</div>
+            <input type="number" name="preco" step="0.01" required class="input input-info" placeholder="Price">
+
+            <input type="file" name="foto" required class="file-input file-input-info">
+
+            <div class="flex justify-start space-x-2">
+                <button type="submit" class="btn btn-outline btn-info w-25 hover:text-white">Edit</button>
+                <a href="{{ route('dashboard') }}" class="btn btn-outline w-25">Back</a>
+            </div>
+        </form>
+    </div>
 
 @endsection
