9d6c4404bbef4e4957ab06858fb67d7833a40131
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-18 22:35:15 -0300

    Update menu.blade.php

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -9,14 +9,14 @@
 
 
             <li class="list-row">
-                <div><img class="size-10 rounded-box" src="{{ $produto->foto }}" /></div>
+                <div><img class="size-10 rounded-box" src="{{asset('storage/' . $produto->foto)}}" /></div>
                 <div>
                     <div class="text-xs uppercase font-semibold opacity-60">{{ $produto->titulo }}</div>
                     <div>{{ $produto->descricao }}</div>
                 </div>
-                <button class="btn btn-square btn-ghost">
+                <a class="btn btn-square btn-ghost" href="{{ route('edit.product', $produto->id) }}">
                     <x-heroicon-s-pencil class="w-6 h-6 text-yellow-500" />
-                </button>
+                </a>
 
                 <form action="{{ route('product.delete', $produto) }}" method="post">
                     @csrf
@@ -29,5 +29,6 @@
 
             </li>
         </ul>
+        <a class="btn btn-outline" href="{{ route('dashboard') }}">Back</a>
     @endforeach
 @endsection
