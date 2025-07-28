172a91d1c9930eb9789ed884c2fbdabe8f8f8453
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2025-07-28 11:37:09 -0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'mercadopago' => [
+        'access_token' => env('MERCADO_PAGO_ACCESS_TOKEN'),
+    ],
 
 ];
